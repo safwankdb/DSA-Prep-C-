2237e0543d9137c5cc99c49e7c0eea17d46626de
safwankdb <kdbeatbox@gmail.com>
2021-03-10 20:14:33 +0530

    beautify

--- Chain of Pairs.cpp
+++ Chain of Pairs.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 int solve(vector<vector<int>> &A) {
     int i, j, ans, m = 0;
-    vector<int> lis;
+    vector<int> LIS;
     for (i = 0; i < A.size(); i++) {
         ans = 0;
         for (j = 0; j < i; j++)
-            if (A[j][1] < A[i][0]) ans = max(ans, lis[j]);
-        lis.push_back(ans + 1);
-        m = max(m, lis[i]);
+            if (A[j][1] < A[i][0]) ans = max(ans, LIS[j]);
+        LIS.push_back(ans + 1);
+        m = max(m, LIS[i]);
     }
     return m;
 }
